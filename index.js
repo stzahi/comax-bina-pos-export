c8bbe3e6bef3e77a4c16b7c65d2aa8aa3f452ae8
Tzahi Serruya <stzahi@gmail.com>
2023-06-16 10:34:26 +0300

    fix env inside axios command

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const puppeteer = require("puppeteer");
+
 (async () => {
   const browser = await puppeteer.launch({
-    args: ['--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox'],
+    args: [
+      "--disable-web-security",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+    ],
   });
   const page = await browser.newPage();
   console.log("\n\n STARTED \n");
@@ -22,72 +27,77 @@ const puppeteer = require("puppeteer");
   await page.waitForSelector(".charts-filters");
 
   console.log("Connected! fetching data");
-  await page.evaluate(() => {
-
-    fetch(
-      "https://rms.comax.co.il/sales/customers/list/?order=0&active=all&start=0&length=1000000&addLog=true",
-      {
-        headers: {
-          accept: "*/*",
-          "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-          "sec-ch-ua":
-            '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Linux"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest",
-        },
-        referrer: "https://rms.comax.co.il/",
-        referrerPolicy: "strict-origin-when-cross-origin",
-        body: null,
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      }
-    ).then((res) => {
-      res.json().then((data) => {
-        fetch("process.env.ALL_CUSTOMERS_HOOK", {
-          body: JSON.stringify(data),
-          method: "POST",
+  let firstHook = process.env.ALL_CUSTOMERS_HOOK;
+  let secondHook = process.env.ALL_INVOICES_HOOK;
+  await page.evaluate(
+    (firstHook, secondHook) => {
+      fetch(
+        "https://rms.comax.co.il/sales/customers/list/?order=0&active=all&start=0&length=1000000&addLog=true",
+        {
+          headers: {
+            accept: "*/*",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "sec-ch-ua":
+              '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+          },
+          referrer: "https://rms.comax.co.il/",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: null,
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+        }
+      ).then((res) => {
+        res.json().then((data) => {
+          fetch(firstHook, {
+            body: JSON.stringify(data),
+            method: "POST",
+          });
         });
       });
-    });
 
-    return fetch(
-      "https://rms.comax.co.il/sales/invoices/list/?type=invoice_receipt&order=1&sorting=datedoc&location=371&start=0&length=1000000&addLog=true",
-      {
-        headers: {
-          accept: "*/*",
-          "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-          "sec-ch-ua":
-            '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Linux"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest",
-        },
-        referrer: "https://rms.comax.co.il/",
-        referrerPolicy: "strict-origin-when-cross-origin",
-        body: null,
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      }
-    ).then((res) => {
-      res.json().then((data) => {
-        fetch("process.env.ALL_INVOICES_HOOK", {
-          body: JSON.stringify(data),
-          method: "POST",
+      return fetch(
+        "https://rms.comax.co.il/sales/invoices/list/?type=invoice_receipt&order=1&sorting=datedoc&location=371&start=0&length=1000000&addLog=true",
+        {
+          headers: {
+            accept: "*/*",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "sec-ch-ua":
+              '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+          },
+          referrer: "https://rms.comax.co.il/",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: null,
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+        }
+      ).then((res) => {
+        res.json().then((data) => {
+          fetch(secondHook, {
+            body: JSON.stringify(data),
+            method: "POST",
+          });
         });
       });
-    });
-  });
+    },
+    firstHook,
+    secondHook
+  );
 
   console.log("DONE");
   await page.waitForNetworkIdle();
   await browser.close();
-})();
+})(process.env.ALL_CUSTOMERS_HOOK, process.env.ALL_INVOICES_HOOK);
